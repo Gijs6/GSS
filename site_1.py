d5985d1bc6d4f5ec99afd4f6540d21b609253b7e
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-02-20 13:47:42 +0100

    Result on a diffrent url

--- site_1.py
+++ site_1.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from main import mainGSS
 
 app = Flask(__name__)
 
-@app.route("/GSS", methods=["GET", "POST"])
-@app.route("/gss", methods=["GET", "POST"])
-def gss():
+
+@app.route("/GSS/result", methods=["GET", "POST"])
+@app.route("/gss/result", methods=["GET", "POST"])
+def gss_result():
     if request.method == "POST":
         htmlinput = request.form.get("htmlinput", "")
         cssinput = request.form.get("cssinput", "")
@@ -14,7 +15,13 @@ def gss():
 
         return render_template("gss.html", cssresult=cssfunctoutput)
     else:
-        return render_template("gss.html")
+        return redirect("/gss")
+
+
+@app.route("/GSS")
+@app.route("/gss")
+def gss():
+    return render_template("gss.html")
 
 
 
